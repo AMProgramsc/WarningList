e6d9f2380c405fc77014aa8db10f51201051408a
Alexander <71174877+AMProgramms@users.noreply.github.com>
2021-05-06 17:59:58 +0400

    Add files via upload

---
+++ WarningList all versions/WarningListCsharp/InterfaceWl/Form2.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace InterfaceWl
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Testbox = new System.Windows.Forms.ListBox();
+            this.Result = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-2, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(803, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Testbox
+            // 
+            this.Testbox.FormattingEnabled = true;
+            this.Testbox.Location = new System.Drawing.Point(-2, 27);
+            this.Testbox.Name = "Testbox";
+            this.Testbox.Size = new System.Drawing.Size(120, 420);
+            this.Testbox.TabIndex = 1;
+            this.Testbox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Result
+            // 
+            this.Result.FormattingEnabled = true;
+            this.Result.Location = new System.Drawing.Point(250, 28);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(37, 420);
+            this.Result.TabIndex = 3;
+            this.Result.SelectedIndexChanged += new System.EventHandler(this.Result_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(293, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 420);
+            this.listBox1.TabIndex = 4;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "copral.jpg");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(124, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Testbox);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "цы";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Testbox;
+        private System.Windows.Forms.ListBox Result;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ WarningList all versions/WarningListCsharp/WarningListCsharp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarningListCsharp
+{
+    class Program
+    {
+        static void intro()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("|WarningList 0.2.1 BASIC|");
+            Console.WriteLine("|Created AMPrograms     |");
+            Console.WriteLine("-------------------------");
+        }
+        enum Score {two = 2, three , four , five };
+        static void Main(string[] args)
+        {
+            Score score;
+            int arrx = 0;
+            double Count = 0;
+            double result;
+            double[] totalryr = new double[14];
+            double totalresult;
+            string [] arr = { "Match: ", "Rus.Iaz: ", "Physics: ", "History: ", "Eng.Iaz: ", "Chemistry: ", "Literature: ", "Socialscience: ", "Biology: ", "Geography: ", "LSF: ", "PE: ", "PD: ", "Informatics: " } ;
+            intro();
+          
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                Console.WriteLine("Enter quality array: ");
+                arrx = int.Parse(Console.ReadLine());
+                int[] size = new int[arrx];
+                for (int c = 0; c < arrx; c++)
+                {
+                    Console.WriteLine("Enter score");
+                    size[c] = int.Parse(Console.ReadLine());
+                    switch(size[c])
+                    {
+                        case 2:
+                           score = Score.two;
+                            break;
+                        case 3:
+                            score = Score.three;
+                            break;
+                        case 4:
+                            score = Score.four;
+                            break;
+                        case 5:
+                            score = Score.five;
+                            break;
+                    }
+                    
+                    if (size[c] == 1 || size[c] > 5)
+                    {
+                        Console.WriteLine("Error! Wrong number!");
+                        return;
+                    }
+                    
+                    Count += size[c];
+                }
+                result = Count / arrx;
+
+                totalryr[i] = result;
+                if (totalryr[i] <= 2.5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your middle score = " + result);
+                    Console.WriteLine("WARNING!!! Low-score!");
+                    Console.ResetColor();
+                }
+                else if (totalryr[i] <= 3.5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Your middle score = " + result);
+                    Console.WriteLine("Warning! Score equal 3! Geting 4 for total score 4");
+                    Console.ResetColor();
+                }
+                else if (totalryr[i] <= 4.5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your middle score = " + result);
+                    Console.WriteLine("Warning! Score equal 4! Geting 5 for total score 5");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Your middle score = " + result);
+                    Console.WriteLine("Total score is normal! ;)");
+                    Console.ResetColor();
+                }
+                Count = 0;
+            }
+            totalresult = (totalryr[0] + totalryr[1] + totalryr[2] + totalryr[3] + totalryr[4] + totalryr[5] + totalryr[6] + totalryr[7] + totalryr[8] + totalryr[9] + totalryr[10] + totalryr[11] + totalryr[12] + totalryr[13]) / 14;
+            Console.WriteLine("Your total result: " + totalresult);
+        }
+    }
+}
