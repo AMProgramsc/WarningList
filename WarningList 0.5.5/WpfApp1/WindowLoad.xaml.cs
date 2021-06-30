bf91159546007213e272b966217e90454258e782
Alexander <71174877+AMProgramms@users.noreply.github.com>
2021-06-30 20:13:15 +0400

    Add files via upload

---
+++ WarningList 0.5.5/WpfApp1/WindowLoad.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowLoad.xaml
+    /// </summary>
+    public partial class WindowLoad : Window
+    {
+        MainWindow window = new MainWindow();
+      
+        public WindowLoad()
+        {
+            
+            InitializeComponent();
+        
+        }
+        
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.RunWorkerAsync();
+        }
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(50);
+            }
+        }
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+                ProgressLoad.Value = e.ProgressPercentage;
+            if (ProgressLoad.Value == 100)
+            {
+                ProgressLoad.Visibility = Visibility.Hidden;
+                Button2.Visibility = Visibility.Visible;
+                Exiter.Visibility = Visibility.Visible;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        
+            Close();
+            window.Show();
+
+        }
+
+        private void Exiter_Click(object sender, RoutedEventArgs e)
+        {
+            window.Close();
+            Close();
+        }
+
+        private void ProgressLoad_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
