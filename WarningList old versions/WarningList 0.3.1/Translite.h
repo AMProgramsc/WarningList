79df51b01086e761d98e8ece156afed31d975d78
Alexander <71174877+AMProgramms@users.noreply.github.com>
2021-07-19 12:57:40 +0400

    Add files via upload

---
+++ WarningList old versions/WarningList 0.3.1/Translite.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+
+
+#pragma warning(disable : 4996);
+using namespace std;
+
+int Translite();
+void SytColor(int d, int s);
+int main();
